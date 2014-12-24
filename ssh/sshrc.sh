18e3c984b3cb82ebccad79d30726b4d5bc03ff84
tianyu <xdtianyu@gmail.com>
2014-12-24 12:39:03 +0800

    Update sshrc.sh

--- ssh/sshrc.sh
+++ ssh/sshrc.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
- 
-NAME="xxx"
-EMAIL="xxx@gmail.com"
- 
-read -d " " ip <<< $PAM_RHOST
+read -d " " ip <<< $SSH_CONNECTION
+#date=$(date "+%d.%m.%Y %Hh%M")
+#reverse=$(dig -x $ip +short)
 geo=$(curl -s http://ip.xdty.org -X POST -d "geo=$ip")
 if [ -z "$geo" ];then
     geo="unknown"
 fi
 ip="$ip($geo)"
- 
-if [ $PAM_TYPE = "close_session" ];then
-    EVENT="logout"
-elif [ $PAM_TYPE = "open_session" ];then
-    EVENT="login"
-fi
- 
-curl -s https://www.xdty.org/mail.php -X POST -d "event=($PAM_USER) $EVENT from $ip&name=$NAME&email=$EMAIL" &
+curl -s https://www.xdty.org/mail.php -X POST -d "event=($USER) login from $ip&name=vps&email=xxx@gmail.com" &
