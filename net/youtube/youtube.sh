3ffb67fc3b4209c5f615af16e5b26a4d2f60330f
tianyu <xdtianyu@gmail.com>
2015-07-22 23:10:49 +0800

    Update youtube.sh

--- net/youtube/youtube.sh
+++ net/youtube/youtube.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+USER="HTTP_USER"
+PASSWORD="HTTP_PASSWORD"
+RPC="https://home.example.com/jsonrpc"
+TOKEN="ARIA2_TOKEN"
+
+DOWNLOAD_URLS=(
+    https://cdn1.example.com/downloads/
+    https://cdn2.example.com/downloads/
+    https://cdn3.example.com/downloads/
+    https://cdn4.example.com/downloads/
+)
+
 URI=$1
 
 export LC_ALL=en_US.UTF-8
@@ -17,7 +29,15 @@ IFS=$(echo -en "\n\b")
 FILES=($(ls -- *$NAME*))
 
 for FILE in "${FILES[@]}"; do
-    echo $FILE
+    LINK=""
+    for URL in "${DOWNLOAD_URLS[@]}"; do
+        if [ -z "$LINK" ]; then
+            LINK="\"$URL$FILE\""
+        else
+            LINK="$LINK, \"$URL$FILE\""
+        fi
+    done
+    curl -v --user $USER:$PASSWORD $RPC -X POST -d "[{\"jsonrpc\":\"2.0\",\"method\":\"aria2.addUri\",\"id\":1,\"params\":[\"token:$TOKEN\",[$LINK],{\"split\":\"10\",\"max-connection-per-server\":\"10\",\"seed-ratio\":\"1.0\"}]}]"
 done
 
 IFS=$SAVEIFS
