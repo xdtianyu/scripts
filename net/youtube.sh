fe2ac781957b4705b397a93e7ef803a811ee9707
tianyu <xdtianyu@gmail.com>
2015-07-22 12:32:46 +0800

    Update youtube.sh

--- net/youtube.sh
+++ net/youtube.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# pip install youtube-dl
-
 URI=$1
+
+export LC_ALL=en_US.UTF-8
 
 AUDIO=$(youtube-dl -F $URI | grep "DASH audio"|grep "aac"|tail -1|cut -d ' ' -f 1)
 VIDEO=$(youtube-dl -F $URI | grep "DASH video"|grep "mp4"|tail -1|cut -d ' ' -f 1)
 
-youtube-dl -f $VIDEO+$AUDIO -k $URI
+youtube-dl -v -f $VIDEO+$AUDIO -k $URI
+
+NAME=$(echo "$URI" |cut -d '=' -f 2)
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+FILES=($(ls -- *$NAME*))
+
+for FILE in "${FILES[@]}"; do
+    echo $FILE
+done
+
+IFS=$SAVEIFS
