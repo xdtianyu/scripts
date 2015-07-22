a6b0c2eeec4d6ab4e0f73b8a5b94e639936cedb9
tianyu <xdtianyu@gmail.com>
2015-07-22 12:39:22 +0800

    Create youtube.php

---
+++ net/youtube/youtube.php
@@ -0,0 +1,26 @@
+<?php
+    $method = $_SERVER['REQUEST_METHOD'];
+    switch ($method) {
+        case 'POST':
+            $input = isset($_POST['urls'])?$_POST['urls']:"";
+            if (strlen($_POST['urls'])!=0) {
+                $urls = explode("\n", str_replace("\r", "", $input));
+                echo json_encode($urls, JSON_UNESCAPED_SLASHES);
+                #shell_exec("echo '".json_encode($urls)."' >>/tmp/out.txt 2>&1 &");
+                foreach ($urls as $url) {
+                    shell_exec("cd /home/downloads;./youtube '".$url."' >>/tmp/youtube.txt 2>&1 &");
+                }
+                exit;
+            } else {
+                echo json_encode(array("error" => "empty url"));
+            }
+            break;
+        case 'GET':
+            echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">
+    URL:<br/> 
+    <textarea name="urls" rows="5" cols="50"></textarea><br/>
+    <input type="submit">
+</form>';
+            break;
+    }
+?>
