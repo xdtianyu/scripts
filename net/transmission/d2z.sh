33532630bb76dc75e6c1d37d3d4a40c4528f55df
tianyu <xdtianyu@gmail.com>
2015-07-25 08:28:02 +0800

    Create d2z.sh

---
+++ net/transmission/d2z.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+X2T=/etc/transmission-daemon/x2t.sh
+ARIA2_RPC=/etc/transmission-daemon/aria2-rpc.sh
+
+export LC_ALL=en_US.UTF-8
+
+if [ "$#" -eq 2 ]; then
+    DIR=$(basename "$2")
+    OPT="$1"
+else
+    DIR=$(basename "$1")
+    OPT="-o"
+fi
+
+zip -r -0 "$DIR.zip" "$DIR"
+
+if [ "$OPT" = "-z" ]; then
+    mv "$DIR" "$DIR-tmp"
+    $X2T -z "$DIR.zip"
+    rm "$DIR.zip"
+    mv "$DIR-tmp" "$DIR"
+else
+    $ARIA2_RPC "$DIR.zip"
+fi
