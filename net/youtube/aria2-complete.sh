e71eee1b6d63b052523e4dbcad7427d5c2e411bd
tianyu <xdtianyu@gmail.com>
2015-08-09 10:09:45 +0800

    Update aria2-complete.sh

--- net/youtube/aria2-complete.sh
+++ net/youtube/aria2-complete.sh
@@ -7,4 +7,4 @@ API_URI=https://remote.example.com/rm/
 echo "$3" >>/tmp/aria2-complete.txt
 
 FILENAME=$(basename "$3")
-curl -s -v --user $USER:$PASSWORD $API_URI -X POST -d "files=$FILENAME" --capath /etc/ssl/certs/ >>/tmp/aria2.txt 2>&1
+curl -s -v --user $USER:$PASSWORD $API_URI -X POST --data-urlencode "files=$FILENAME" --capath /etc/ssl/certs/ >>/tmp/aria2.txt 2>&1
