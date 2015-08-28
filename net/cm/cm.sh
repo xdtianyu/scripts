b3938d26573d710045b0c0ad4a25b92ab515f233
tianyu <xdtianyu@gmail.com>
2015-08-28 10:06:27 +0800

    Update cm.sh

--- net/cm/cm.sh
+++ net/cm/cm.sh
@@ -6,6 +6,6 @@ DEVICE=$3
 
 mkdir -p $DIR/log
 /root/bypy/bypy.py mkdir cm/$DEVICE >> $DIR/log/$DEVICE.log 2>&1
-/root/bypy/bypy.py -v --disable-ssl-check -s 10MB upload $DIR/$FILE cm/$DEVICE/$FILE >> $DIR/log/$DEVICE.log 2>&1
+trickle -s -u 2000 /root/bypy/bypy.py -v --disable-ssl-check -s 10MB upload $DIR/$FILE cm/$DEVICE/$FILE >> $DIR/log/$DEVICE.log 2>&1
 
 rm $DIR/$FILE >> $DIR/log/$DEVICE.log 2>&1
