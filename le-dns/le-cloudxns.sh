e537108958b42f17227f5fa753e691b179837bbd
tianyu <xdtianyu@gmail.com>
2016-02-02 13:07:01 +0800

    Update le-cloudxns.sh

--- le-dns/le-cloudxns.sh
+++ le-dns/le-cloudxns.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CONFIG=$1
+
 if [ ! -f "$CONFIG" ];then
     echo "ERROR, CONFIG NOT EXIST."
     exit 1
@@ -7,6 +9,21 @@ fi
 
 . $CONFIG
 
-echo $DOMAINS > domains.txt
+echo "$CERT_DOMAINS" > domains.txt
+
+if [ ! -f "cloudxns.sh" ];then
+    wget https://github.com/xdtianyu/scripts/raw/master/le-dns/cloudxns.sh -O cloudxns.sh -o /dev/null
+    chmod +x cloudxns.sh
+fi
+
+if [ ! -f "cloudxns-hook.sh" ];then
+    wget https://github.com/xdtianyu/scripts/raw/master/le-dns/cloudxns-hook.sh -O cloudxns-hook.sh -o /dev/null
+    chmod +x cloudxns-hook.sh
+fi
+
+if [ ! -f "letsencrypt.sh" ];then
+    wget https://github.com/lukas2511/letsencrypt.sh/raw/master/letsencrypt.sh -O letsencrypt.sh -o /dev/null
+    chmod +x letsencrypt.sh
+fi
 
 ./letsencrypt.sh -c -k ./cloudxns-hook.sh -t dns-01
