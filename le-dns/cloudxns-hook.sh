809424afc6bb08da2819a72569e8c6e2dc4347bd
tianyu <xdtianyu@gmail.com>
2016-02-17 10:13:12 +0800

    Update cloudxns-hook.sh

--- le-dns/cloudxns-hook.sh
+++ le-dns/cloudxns-hook.sh
@@ -3,7 +3,7 @@
 function deploy_challenge {
     local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
     echo "$DOMAIN" "$TOKEN_FILENAME" "$TOKEN_VALUE"
-    ./cloudxns.sh ./cloudxns.conf "$DOMAIN" "$TOKEN_VALUE"
+    ./cloudxns.sh "$CONFIG" "$DOMAIN" "$TOKEN_VALUE"
     sleep 5
 }
 
