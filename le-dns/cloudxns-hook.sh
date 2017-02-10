51e83b1d758bec0d4ad39c0cef96eba87cb60fa6
tianyu <xdtianyu@gmail.com>
2017-02-10 16:49:07 +0800

    Update cloudxns-hook.sh

--- le-dns/cloudxns-hook.sh
+++ le-dns/cloudxns-hook.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-function deploy_challenge {
+deploy_challenge() {
     local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
     echo "$DOMAIN" "$TOKEN_FILENAME" "$TOKEN_VALUE"
     ./cloudxns.sh "$CONFIG" "$DOMAIN" "$TOKEN_VALUE"
     sleep 5
 }
 
-function clean_challenge {
+clean_challenge() {
     local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 }
 
-function deploy_cert {
-    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" CHAINFILE="${4}"
+deploy_cert() {
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
 }
 
-function unchanged_cert {
+unchanged_cert() {
     local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
 }
 
-HANDLER=$1; shift; $HANDLER $@
+invalid_challenge() {
+    local DOMAIN="${1}" RESPONSE="${2}"
+}
+
+request_failure() {
+    local STATUSCODE="${1}" REASON="${2}" REQTYPE="${3}"
+}
+
+exit_hook() {
+
+  :
+}
+
+HANDLER="$1"; shift
+if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge|deploy_cert|unchanged_cert|invalid_challenge|request_failure|exit_hook)$ ]]; then
+  "$HANDLER" "$@"
+fi
